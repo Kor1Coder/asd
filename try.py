adf2e7c49529d64aef646c7ed0e777f11de262cd
asus <huba.huba_06@hotma>
2023-08-29 16:43:43 +0300

    initial commiter

--- try.py
+++ try.py
@@ -1,2 +1,7 @@
-import time
-print(time.strftime("%A"))
+import PySimpleGUI
+label=PySimpleGUI.Text("YAZ BURAYA")
+inp_box=PySimpleGUI.InputText(tooltip="enter ")
+addbutton=PySimpleGUI.Button("TERT")
+win=PySimpleGUI.Window('GUI',layout=[[label,inp_box,addbutton]])
+win.read()
+win.close()
